f6848d1d5fd27b812236a6e8cf9d0731b7b939a0
akgjohansson <johansson.mcquack@gmail.com>
2017-09-19 10:40:10 +0200

    Added test project

---
+++ TestUserInput/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserInput;
+
+namespace TestUserInput
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestWriteAsFraction()
+        {
+           string output = InputCleanup.WriteAsFraction(1f / 6f);
+        }
+    }
+}
