37d2c61afb67b9aaa313287d1f2fa24677efff42
akgjohansson <johansson.mcquack@gmail.com>
2017-09-20 10:34:50 +0200

    Moved WriteAsFraction to project MathToolBox

--- UserInput/InputCleanup.cs
+++ UserInput/InputCleanup.cs
@@ -10,29 +10,7 @@ namespace UserInput
     {
         public static string WriteAsFraction(float input)
         {
-            string output = "";
-            if (input < 0)
-            {
-                output += "-";
-                input *= -1;
-            }
-            if (input > 1)
-            {
-                int floor = Convert.ToInt32(Math.Floor(input));
-                output += $"{Convert.ToString(floor)} ";
-                input -= floor;
-            }
-            for (int denominator = 2; denominator < 200; denominator++)
-            {
-                float numerator = input * denominator;
-                float difference = Math.Abs(numerator - (float)Math.Round(numerator));
-                if (difference < 0.001*input)
-                {
-                    output += $"{numerator}/{denominator}";
-                    return output;
-                }
-            }
-            return null;
+            return MathToolBox.Fractions.FractionToString(input);
         }
     }
 }
